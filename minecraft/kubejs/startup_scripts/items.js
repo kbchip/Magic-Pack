d2cf57bd630e90c8fc27880996e48ba4d2236754
pyrxtic <139200414+pyrxtic@users.noreply.github.com>
2025-01-17 16:09:53 -0600

    bundle fix

--- minecraft/kubejs/startup_scripts/items.js
+++
@@ -1,7 +0,0 @@
-// Item registry event listener
-StartupEvents.registry('item', event => {
-    // Register bundle in item index
-event.create('minecraft:bundle')
-    .maxStackSize(1)
-    .containerItem('minecraft:bundle')
-})
