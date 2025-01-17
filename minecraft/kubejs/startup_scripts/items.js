3c3c8f991bd530b8a165834fe60e0b05454d6990
pyrxtic <139200414+pyrxtic@users.noreply.github.com>
2025-01-17 15:07:26 -0600

    infinite alchemist cauldron fix

--- minecraft/kubejs/startup_scripts/items.js
+++ minecraft/kubejs/startup_scripts/items.js
@@ -1,5 +1,7 @@
 // Item registry event listener
 StartupEvents.registry('item', event => {
     // Register bundle in item index
-    event.create('minecraft:bundle')
+event.create('minecraft:bundle')
+    .maxStackSize(1)
+    .containerItem('minecraft:bundle')
 })
